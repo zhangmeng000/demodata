36210b4debcb6ade59d60c882d7340a56fd61c0e
zhangmeng000 <1550685677@qq.com>
2016-09-18 14:38:24 +0800

    card

---
+++ Card.js
@@ -0,0 +1,7 @@
+let card = [
+  {"title":"第一天","desc":"第一天描述","img":"http://obmf232cc.bkt.clouddn.com/home1.jpg","url":"a"},
+  {"title":"第二天","desc":"第二天描述","img":"http://obmf232cc.bkt.clouddn.com/home1.jpg","url":"b"},
+  {"title":"第三天","desc":"第三天描述","img":"http://obmf232cc.bkt.clouddn.com/home1.jpg","url":"c"},
+  {"title":"第四天","desc":"第四天描述","img":"http://obmf232cc.bkt.clouddn.com/home1.jpg","url":"d"},
+  {"title":"第五天","desc":"第五天描述","img":"http://obmf232cc.bkt.clouddn.com/home1.jpg","url":"e"},
+]
